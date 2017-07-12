7564075612ecaa54d117c5972430461510668af3
astro <m.rahm7n@gmail.com>
2017-07-12 17:26:16 +0200

    Update crts_controller.cpp

--- src/crts_controller.cpp
+++ src/crts_controller.cpp
@@ -21,13 +21,7 @@
 #include "extensible_cognitive_radio.hpp"
 
 // EDIT INCLUDE START FLAG
-#include "../scenario_controllers/SC_Performance_Sweep_Utility/SC_Performance_Sweep_Utility.hpp"
-#include "../scenario_controllers/SC_BER_Sweep/SC_BER_Sweep.hpp"
 #include "../scenario_controllers/SC_Template/SC_Template.hpp"
-#include "../scenario_controllers/SC_Network_Loading/SC_Network_Loading.hpp"
-#include "../scenario_controllers/SC_CORNET_Display/SC_CORNET_Display.hpp"
-#include "../scenario_controllers/SC_Control_and_Feedback_Test/SC_Control_and_Feedback_Test.hpp"
-#include "../scenario_controllers/SC_CORNET_Tutorial/SC_CORNET_Tutorial.hpp"
 // EDIT INCLUDE END FLAG
 
 #define MAXPENDING 5
@@ -108,20 +102,9 @@ ScenarioController* create_sc(struct scenario_parameters *sp){
   ScenarioController *SC;
 
   // EDIT SET SC START FLAG
-  if(!strcmp(sp->SC, "SC_Performance_Sweep_Utility"))
-    SC = new SC_Performance_Sweep_Utility(argc, argv);
-  if(!strcmp(sp->SC, "SC_BER_Sweep"))
-    SC = new SC_BER_Sweep(argc, argv);
   if(!strcmp(sp->SC, "SC_Template"))
     SC = new SC_Template(argc, argv);
-  if(!strcmp(sp->SC, "SC_Network_Loading"))
-    SC = new SC_Network_Loading(argc, argv);
-  if(!strcmp(sp->SC, "SC_CORNET_Display"))
-    SC = new SC_CORNET_Display(argc, argv);
-  if(!strcmp(sp->SC, "SC_Control_and_Feedback_Test"))
-    SC = new SC_Control_and_Feedback_Test(argc, argv);
-  if(!strcmp(sp->SC, "SC_CORNET_Tutorial"))
-    SC = new SC_CORNET_Tutorial(argc, argv);
+
   // EDIT SET SC END FLAG
     
   freeargcargv(argc, argv);
